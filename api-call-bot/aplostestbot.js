a6c8090577b2c9712e57943eedfa7a0ca9ec673a
ashay-maheshwwari <aaryaashay1848@gmail.com>
2019-01-14 14:03:55 +0530

    working sample bot

---
+++ api-call-bot/aplostestbot.js
@@ -0,0 +1,101 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+var http = require('http');
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('listening to %s', server.url);
+});
+
+var connector = new builder.ChatConnector({
+    appId: null,
+    appPassword: null
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+/*bot.dialog('/', function (session, args) {
+    session.send("Hi");
+    console.log(session.message.text);
+});*/
+
+bot.dialog('/', function (session, args) {
+    if (!session.userData.greeting) {
+        session.send("Hello. What is your name?");
+        session.userData.greeting = true;
+    } else if (!session.userData.name) {
+        getName(session);
+    } else if (!session.userData.email) {
+        getEmail(session);
+    } else if (!session.userData.password) {
+        getPassword(session);
+    } else {
+        session.userData = null;
+    }
+    session.endDialog();
+});
+
+function getName(session) {
+    name = session.message.text;
+    session.userData.name = name;
+    session.send("Hello, " + name + ". What is your Email ID?");
+}
+function getEmail(session) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    email = session.message.text;
+    if (re.test(email)) {
+        session.userData.email = email;
+        session.send("Thank you, " + session.userData.name + ". Please set a new password.");
+    } else {
+        session.send("Please type a valid email address. For example: test@hotmail.com");
+    }
+}
+
+function getPassword(session) {
+    var re = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+    password = session.message.text;
+    if (re.test(password)) {
+        session.userData.password = password;
+        var data = session.userData;
+        sendData(data, function (msg) {
+            session.send(msg);
+            session.userData = null;
+        });
+    } else {
+        session.send("Password must contain at least 8 characters, including at least 1 number, 1 uppercase letter, 1 lowercase letter and 1 special character. For example: Mybot@123");
+    }
+}
+
+function sendData_1(data, cb) {
+   /*var obj = fetch('https://jsonplaceholder.typicode.com/todos/1')
+   .then(response => response.json())
+   .then(data => obj = data)
+   .then(() => console.log(obj))*/
+   http.get("https://jsonplaceholder.typicode.com/todos/1",  function (res) {
+       var msg = '';
+       res.on("data", function (chunk) { 
+           msg += chunk;
+       });
+
+        res.on('end', function(){
+            cb(msg);
+        })
+   })
+}
+
+
+function sendData(data, cb) {
+    http.get("http://local.dev/aplostestbot/saveData.php?name=" + data.name + "&email=" + data.email + "&password=" + data.password, function (res) {
+        var msg = '';
+        res.on("data", function (chunk) {
+            msg += chunk;
+        });
+
+        res.on('end', function () {
+            cb(msg);
+        });
+
+    }).on('error', function (e) {
+        console.log("Got error: " + e.message);
+    });
+}
